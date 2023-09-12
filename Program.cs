b80d4775b78baac242b56e762b6288466d0f9a73
Banson <97927045+bansonsy@users.noreply.github.com>
2023-09-12 16:00:42 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Banson Sy
+// 8/3/23
+// Mini Challenge 1: Say Hello 
+// Tasked with creating a console project asking for my name. I'll then have to input my name and print "Hello <Name>"
+
+// Peer Review: Ajay Gill. Code looks good and everything functions. Good job. 
+
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+Console.WriteLine("Hello " + name + ".");
